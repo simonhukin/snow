9ac866ea7203fef1e60378e2aa79c87867944e83
Simon Hukin <simonhukin@gmail.com>
2011-01-31 10:21:45 +1100

    New Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snow::Application.config.secret_token = 'c8cb6a79c53d94f20ab756ee675849746f86b6ec219233b431131d5be9e11c24b741b1b07fa1cf252c32b1b4bda8efa4d392e260c8cb687a006881c7e8586317'
